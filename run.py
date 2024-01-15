2956285aaa73bca974f0dfae3912cac2c6e85c38
hassam722 <hassamghori722@gmail.com>
2024-01-15 19:09:01 +0500

    first commit

---
+++ run.py
@@ -0,0 +1,51 @@
+import sys,os,socket
+import subprocess
+from venv import EnvBuilder
+from pathlib import Path
+
+ipaddress  = socket.gethostbyname(socket.gethostname())
+port = 3003
+
+curr_dir = os.getcwd()
+
+def run_server(daphne_path):
+    python_path = curr_dir+"\\venv\\Scripts\\python.exe"
+    subprocess.run([python_path,daphne_path, '-b',ipaddress,'-p',str(port),"CIO_MACHINE_SERVER.asgi:application"], check=True)    
+
+class CustomEnvBuilder(EnvBuilder):
+    """
+    Subclass of venv.EnvBuilder to customize the virtual environment creation process.
+    """
+
+    def create(self, env_dir):
+        """
+        Override the create method to install additional packages after the default virtual environment is created.
+        """
+        super().create(env_dir)
+
+        # Additional packages to install in the virtual environment
+        requirement_file = curr_dir + "\\requirements.txt"
+        
+        print("venv created")
+        pip_path = curr_dir+"\\venv\\Scripts\\pip.exe"
+        # Install additional packages using pip
+        subprocess.run([pip_path, 'install',"-r",requirement_file], check=True)
+        daphne_path = curr_dir+"\\venv\\Scripts\\daphne.exe"
+        run_server(daphne_path)
+
+if __name__ == "__main__":
+    # Specify the virtual environment directory
+    venv_dir = curr_dir + "\\venv"
+    venv_path = Path(sys.prefix) / venv_dir
+    
+    # Check if the virtual environment already exists
+    if not (venv_path).is_dir():
+        print("Creating virtual environment...")
+        # Use CustomEnvBuilder to customize the virtual environment creation process
+        builder = CustomEnvBuilder(with_pip=True)
+        builder.create(venv_dir)
+        print("Virtual environment created.")
+    else:
+        print("Virtual environment already exists.")
+        daphne_path = curr_dir+"\\venv\\Scripts\\daphne.exe"
+        run_server(daphne_path)
